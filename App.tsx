7bdf4bfd05b591e07fae5826769bd3f004f31a66
Abrar Al Hasan <abraralhasan111@gmail.com>
2024-02-04 10:14:34 +0530

    Setup React Navigation

--- App.tsx
+++ App.tsx
@@ -4,12 +4,15 @@ import {Provider} from 'react-redux';
 import Index from './src/Screens/Index';
 import {store} from './src/Store';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
   return (
     <Provider store={store}>
       <SafeAreaProvider>
-        <Index />
+        <NavigationContainer>
+          <Index />
+        </NavigationContainer>
       </SafeAreaProvider>
     </Provider>
   );
